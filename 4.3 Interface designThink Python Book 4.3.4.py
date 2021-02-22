bac207835bf8a967a19956cccb8ffa69353612bf
Dawnella <dawnella28@gmail.com>
2021-02-21 16:48:43 -0800

    New version

---
+++ 4.3 Interface designThink Python Book 4.3.4.py
@@ -0,0 +1,28 @@
+"""4. Write a function called circle that takes a turtle, t, and radius, r, as parameters and
+that draws an approximate circle by calling polygon with an appropriate length and
+number of sides. Test your function with a range of values of r.
+Hint: figure out the circumference of the circle and make sure that length * n =
+circumference."""
+
+import math
+import turtle
+
+bob = turtle.Turtle()
+
+# Polygon fun
+
+def polygon(t,length,n):
+    for i in range(30):
+        t.fd(length)
+        t.lt(360/n)
+
+def circle(t,r):
+    circunference = 2.0*math.pi*r
+    n = 30
+    length = circunference/n
+    polygon(t,length,n)
+
+
+circle(bob,80)
+
+turtle.mainloop()
---
+++ 6 Fruitful FunctionThink Python Exercise 6.1.py
@@ -0,0 +1,13 @@
+'''As an exercise, write a compare function takes two values, x and y, and returns 1 if x > y,
+0 if x == y, and -1 if x < y.'''
+
+def compare_function(x,y):
+    if x > y:
+        h = 1
+        return h
+    elif x == y:
+        return 0
+    elif x < y:
+        return -1
+
+compare_function(4,8)
